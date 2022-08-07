32829e875ade302bbd73725526fd425d904320d6
Matic Conradi <matic.conradi@me.com>
2022-08-07 20:14:38 +0200

    Added temporary logging

--- app/alerts.py
+++ app/alerts.py
@@ -77,8 +77,10 @@ class AlertsServer(object):
 	# -------------------------
 
 	async def process_price_alerts(self):
+		s = time()
 		auth_req = google.auth.transport.requests.Request()
 		token = google.oauth2.id_token.fetch_id_token(auth_req, "https://candle-server-yzrdox65bq-uc.a.run.app/")
+		print(time() - s)
 		headers = {
 			"Authorization": "Bearer " + token,
 			"content-type": "application/json",
